ae42a74bba058a4b03a8082c9fcf09afb378ee5a
aosidorov <aosidorov@gmail.com>
2020-03-17 13:40:46 +1100

    cosmetic changes

--- genpwd.ps1
+++ genpwd.ps1
@@ -1,13 +1,16 @@
-$min_char = 13
-$max_char = 18
+#$VerbosePreference = "Continue"
+$min_char = 13                    # the minimum number of characters in the password
+$max_char = 18                    # the maximum number of characters in the password
+$min_words = 1000                 # the minimum number of words in the dictionary
+$pwd_dict_file = "dict.txt"       # the word dictionary
 
 $rand = new-object System.Random
 $conjunction = "the","my","we","our","and","but"
-[string[]]$words = Get-Content -Path dict.txt
+[string[]]$words = Get-Content -Path $pwd_dict_file 
 $count = $words.Count
-Write-Host "Words count: "$count
-if ($count -le 1000) {
-    Write-Host "The dictionaty is too small will return a random password"
+Write-Verbose "Words count: $count"
+if ($count -le $min_words) {
+    Write-Verbose "The dictionaty is too small. Will return a random password"
     $password1 = ([char[]](Get-Random -Input $(65..90) -Count 5)) -join "" 
     $password2 = ([char[]](Get-Random -Input $(35..37) ))  
     $password3 = ([char[]](Get-Random -Input $(48..57) -Count 2)) -join "" 
@@ -17,9 +20,9 @@ if ($count -le 1000) {
         $word1 = $words[$rand.Next(0,$words.Count)]
         $con = ($conjunction[$rand.Next(0,$conjunction.Count)])
         $word2 = $words[$rand.Next(0,$words.Count)]
-        $new_password  = $word1 + " " + $con + " " + $word2
+        $new_password  = $word1 + $con + $word2
         $len = $new_password.Length
-        Write-Host $new_password "($len)" 
+        Write-Verbose "$new_password ($len)" 
     } until (($len -le $max_char) -and ($len -ge $min_char))
 }
 return $new_password
